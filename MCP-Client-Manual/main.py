adac500b51c20a9cb6b84aecfc32b8dac55347af
Paurakh <pandeypaurakhraj@gmail.com>
2025-06-03 12:56:10 +0545

    main entry point

--- MCP-Client-Manual/main.py
+++ MCP-Client-Manual/main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from mcp-client-manual!")
+import asyncio 
+from mcp_client import MCPClient
 
-
+async def main():
+    client=MCPClient()
+    try:
+        await client.connect_to_server(r"D:\MCP\MCP-Client-Manual\test.py")
+        response = await client.session.list_tools()
+        print("\nConnected to server with tools:", [tool.name for tool in response.tools])
+    except Exception as e:
+        print(f"Error connecting to server: {e}")
+        return  None
+    finally:    
+        await client.exit_stack.aclose()
+        
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
